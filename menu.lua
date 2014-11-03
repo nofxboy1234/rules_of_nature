236ea56fc263c0750728a2910778eaccbf7a105c
Dylan Palmboom <nofxboy1234@yahoo.ca>
2014-11-03 23:11:36 +0200

    Added menu.lua from love_sa_2 and changed it into a middleclass class

--- menu.lua
+++ menu.lua
@@ -1,32 +1,32 @@
+local Menu = class('Menu')
 
+function Menu:initialize()
+  self.menu_items = {"start game", "credits", "help", "quit"}
+  self.title_img = love.graphics.newImage("Resources/Images/SA-01_720.png")
 
-function menu_load()
-  menu_items = {"start game", "credits", "help", "quit"}
-  title_img = love.graphics.newImage("Resources/Images/SA-01_720.png")
+  self.music = love.audio.newSource("Resources/Sounds/Circlerun_ZEQ2_Select_Your_Game_Mode.mp3", "stream")
+  self.music:setLooping(true)
+  self.music:setVolume(0.2)
+  self.music:play()
 
-  music = love.audio.newSource("Resources/Sounds/Circlerun_ZEQ2_Select_Your_Game_Mode.mp3", "stream")
-  music:setLooping(true)
-  music:setVolume(0.2)
-  music:play()
-
-  menu_sound = love.audio.newSource("Resources/Sounds/Menu_Selection_Click.wav", "static")
+  self.menu_sound = love.audio.newSource("Resources/Sounds/Menu_Selection_Click.wav", "static")
   -- menu_sound:setVolume(1.0)
 
-  menuselection = 1
+  self.menuselection = 1
 end
 
-function menu_update(dt)
+function Menu:update(dt)
   -- body
 end
 
-function menu_draw()
-  love.graphics.draw(title_img, 0, 0)
+function Menu:draw()
+  love.graphics.draw(self.title_img, 0, 0)
   -- Draw menu
-  center_v_offset = 150
-  offset = 0
-  for i = 1, #menu_items do
+  local center_v_offset = 150
+  local offset = 0
+  for i = 1, #self.menu_items do
     -- Set menu text colour depending on of it is selected
-    if menuselection == i then
+    if self.menuselection == i then
       love.graphics.setColor(255, 255, 255)
     else
       love.graphics.setColor(0, 0, 0)
@@ -39,40 +39,41 @@ function menu_draw()
 
 end
 
-function menu_keypressed(key, isrepeat)
+function Menu:keypressed(key, isrepeat)
   if key == "escape" then
       love.event.quit()
   end
 
-  -- if (key == "up") and menuselection > 1 then
-  --   menuselection = menuselection - 1
-  -- elseif (key == "down") and menuselection < #menu_items then
-  --     menuselection = menuselection + 1
+  -- if (key == "up") and self.menuselection > 1 then
+  --   self.menuselection = self.menuselection - 1
+  -- elseif (key == "down") and self.menuselection < #menu_items then
+  --     self.menuselection = self.menuselection + 1
   -- end
 
-  if (key == "up") and menuselection > 1 then
-    menuselection = menuselection - 1
-    menu_sound:stop()
-    menu_sound:play()
-  elseif (key == "up") and menuselection == 1 then
-    menuselection = #menu_items
-    menu_sound:stop()
-    menu_sound:play()
-  elseif (key == "down") and menuselection < #menu_items then
-    menuselection = menuselection + 1
-    menu_sound:stop()
-    menu_sound:play()
-  elseif (key == "down") and menuselection == #menu_items then
-    menuselection = 1
-    menu_sound:stop()
-    menu_sound:play()
+  if (key == "up") and self.menuselection > 1 then
+    self.menuselection = self.menuselection - 1
+    self.menu_sound:stop()
+    self.menu_sound:play()
+  elseif (key == "up") and self.menuselection == 1 then
+    self.menuselection = #menu_items
+    self.menu_sound:stop()
+    self.menu_sound:play()
+  elseif (key == "down") and self.menuselection < #menu_items then
+    self.menuselection = self.menuselection + 1
+    self.menu_sound:stop()
+    self.menu_sound:play()
+  elseif (key == "down") and self.menuselection == #menu_items then
+    self.menuselection = 1
+    self.menu_sound:stop()
+    self.menu_sound:play()
   end
 
-  if key == "return" and menuselection == 1 then
-    music:stop()
+  if key == "return" and self.menuselection == 1 then
+    self.music:stop()
     changegamestate("game")
-  elseif key == "return" and menuselection == 4 then
+  elseif key == "return" and self.menuselection == 4 then
     love.event.quit()
   end
 end
 
+return Menu
